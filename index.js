c234d886567ff770a90f9bd7b96ca114dd3931fc
Katarzyna Da Silva <conic916@gmail.com>
2019-04-30 22:14:49 +0200

    add folder

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-// implementacja protokolu w postaci modulu http :
-//inicjacja serwera http :
-var http = require('http');
-var server = http.createServer(); //metoda
-// .on - nasluchiwanie na zdarzenie
-server.on('request', function (request, response) {});
-// wykonywanie czynnosci dzieki  request i response
 
-// wysylanie odpowiedzi protokolem http :
+var http = require('http');
+var fs = require('fs');
+var server = http.createServer();
 
 server.on('request', function (request, response) {
-    response.write('Hello world!');
-    response.end();
+    if (request.method === 'GET' && request.url === '/hello world') {
+        response.setHeader('Content-Type', 'text/html; charset=utf-8');
+        // First I want to read the file
+        fs.readFile('./index.html', function (error, data) {
+            if (error) throw error;
+            response.write(data);
+            response.end();
+        });
+    }   else {
+        response.setHeader('Content-Type', 'chatton.jpg; charset=utf-8');
+        fs.readFile('./chatton.jpg', function (error, data) {
+            if (error) throw error;
+            response.statusCode = 404;
+            response.write(data);
+            response.end();
+        });
+    }
 });
 
-// port jaki ma nasluchiwac bez przerwy :
+// port jaki ma nasluchiwac :
 server.listen(9000);
